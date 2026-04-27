4bfaa85270d69c74bf426a64a178d298622a1ad3
Md. Sazid Uddin <43735245+manchitro@users.noreply.github.com>
2026-04-27 22:41:16 +0600

    add device name to fish prompt

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -1,5 +1,20 @@
 function fish_prompt
-    echo -n (set_color brcyan)$USER' '(set_color brmagenta)'in '(set_color brgreen)(prompt_pwd)' '
-    echo (set_color brmagenta)'on'(set_color bryellow)(git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/ (\1)/')
+    # Format: $user on {device} in {directory} [git branch]
+    set -l device (hostname)
+    set -l git_branch (git branch 2> /dev/null | sed -e '/^[^*]/d' -e "s/* \(.*\)/ (\1)/")
+
+    # First line: user on device in directory (include git branch)
+    echo -n (set_color brcyan)$USER' '
+    echo -n (set_color brmagenta)'on '
+    echo -n (set_color bryellow)$device' '
+    echo -n (set_color brmagenta)'in '
+    echo -n (set_color brgreen)(prompt_pwd)''
+    if test -n "$git_branch"
+        echo (set_color brmagenta)$git_branch
+    else
+        echo
+    end
+
+    # Second line: only the prompt glyph
     echo -n (set_color white)'⥂ '
 end
